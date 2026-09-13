afdffabbbacfaf2efd0ac5d88d7662167241c14f
Sipos80 <51282664+Sipos80@users.noreply.github.com>
2026-09-13 10:23:12 +0200

    Update video-cards.js

--- js/video-cards.js
+++ js/video-cards.js
@@ -8,7 +8,7 @@ const videos = [
    {
       id: "SLnCjCTzyK0",
       title: "WHO SHOULD DECIDE",
-      description: "Az Amazon Frontlines zeneszerző versenyére készített díjnyertes pályaművem. Ez egy nemzetközi verseny volt melyen 500-nál is több nevező indult a világ minden tájáról. Bár nem végeztem az első helyen, de nagyon megtisztelő volt részt venni a díjátdón, ahol japán, olasz, francia zeneszerző társaimmal is megismerkedhettem.",
+      description: "Az Amazon Frontlines zeneszerző versenyére készített díjnyertes pályaművem. Ezen a nemzetközi versenyen több, mint 500 nevező közül választott ki a zsűri a fináléba. Bár végül nem végeztem az első helyen, de nagyon megtisztelő volt részt venni a díjátdón, ahol japán, olasz, francia zeneszerző társaimmal is megismerkedhettem.",
       poster: "https://img.youtube.com/vi/SLnCjCTzyK0/hqdefault.jpg"
    },
    {
