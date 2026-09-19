ea0af65d35f9a677b74d2c12bf861c8fc7c578d1
Sipos80 <51282664+Sipos80@users.noreply.github.com>
2026-09-19 16:22:51 +0200

    Update video-cards.js

--- js/video-cards.js
+++ js/video-cards.js
@@ -8,7 +8,7 @@ const videos = [
    {
       id: "SLnCjCTzyK0",
       title: "WHO SHOULD DECIDE",
-      description: "Az Amazon Frontlines zeneszerző versenyére készített díjnyertes pályaművem. Ezen a nemzetközi versenyen többszáz nevező közül választott ki a zsűri a fináléba a legjobb 10 közé.",
+      description: "Az Amazon Frontlines zeneszerző versenyére készített díjnyertes pályaművem. Ezen a nemzetközi versenyen több mint 500 nevező közül választott ki a zsűri a fináléba a legjobb 10 közé.",
       poster: "https://img.youtube.com/vi/SLnCjCTzyK0/hqdefault.jpg"
    },
    {
